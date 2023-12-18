45dccf7540c2353cd01991e48e433744c8471352
metrysaa20 <antonius1999@yahoo.de>
2023-12-18 23:18:59 +0100

    implemented two unit tests for shopping view

---
+++ app/src/androidTest/java/at/fhj/hofer_cart/ShoppingViewTest.kt
@@ -0,0 +1,40 @@
+package at.fhj.hofer_cart
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@OptIn(ExperimentalMaterial3Api::class)
+@RunWith(AndroidJUnit4::class)
+class ShoppingViewTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testShoppingView() {
+        composeTestRule.setContent {
+            ShoppingView()
+        }
+        composeTestRule.onNodeWithText("Shopping List").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Enter item").assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription("Add").assertIsDisplayed()
+    }
+
+    @Test
+    fun testDisplayItem() {
+        composeTestRule.setContent {
+            ShoppingView()
+        }
+        composeTestRule.onNodeWithContentDescription("Add").performClick()
+        composeTestRule.onNodeWithText("Beef MEAT").assertIsDisplayed()
+    }
+}
